5cc791c3b931c30b810330069121efb9b6265c8e
trongtrong <trongnv@9pay.vn>
2023-02-27 12:55:03 +0700

    Fix:sdsd

--- testmodule/src/main/java/com/example/testmodule/MainActivity.java
+++ testmodule/src/main/java/com/example/testmodule/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.testmodule;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView text1 = (TextView) findViewById(R.id.txt_test);
+
+        int a = 10;
+        int b = 20;
+
+        if (a > b){
+            b = 30;
+        }else {
+            a = 20;
+        }
+
+
     }
 }
